8150d35524269ec646b215df83fddc38fa6392a2
Aymane-Guelbaoui <aymaneguelbaoui@gmail.com>
2025-07-10 00:58:58 +0100

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -7,11 +7,26 @@ FROM
 GROUP BY
     user_id;
 
--- Query 2: Rank properties based on total bookings using ROW_NUMBER
+-- Query 2: Rank properties using ROW_NUMBER based on total bookings
 SELECT
     property_id,
     total_bookings,
-    ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS booking_rank
+    ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS row_number_rank
+FROM (
+    SELECT
+        property_id,
+        COUNT(*) AS total_bookings
+    FROM
+        bookings
+    GROUP BY
+        property_id
+) AS property_counts;
+
+-- Query 3: Rank properties using RANK based on total bookings
+SELECT
+    property_id,
+    total_bookings,
+    RANK() OVER (ORDER BY total_bookings DESC) AS rank_position
 FROM (
     SELECT
         property_id,
